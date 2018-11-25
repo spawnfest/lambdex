887074c69da9d1b47b9d923abd19606aab3f5919
Pablo Brudnick <pbrudnick@gmail.com>
2018-11-25 18:35:45 -0300

    passing ecto tasks to root umbrella mix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule Lambdex.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -15,6 +16,20 @@ defmodule Lambdex.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run apps/lambdex_server/priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
+    ]
   end
 end
