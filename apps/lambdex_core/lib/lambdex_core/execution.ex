3436638ec1dc1b56e4c36bb02696537484d51d66
Pablo Brudnick <pbrudnick@gmail.com>
2018-11-25 13:18:06 -0300

    catching erros on execution and saving execution values. closes #14

--- apps/lambdex_core/lib/lambdex_core/execution.ex
+++ apps/lambdex_core/lib/lambdex_core/execution.ex
@@ -1,17 +1,43 @@
 defmodule LambdexCore.Execution do
-@moduledoc """
-The Lambda Execution
-"""
+  @moduledoc """
+  The Lambda Execution
+  """
 
-defstruct lambda_id: nil,
-  executed_at: nil,
-  result: nil,
-  output: "",
-  sign: nil,
-  duration: 0,
-  max_cpu_used: 0,
-  request_id: "",
-  status: nil,
-  error: nil
+  defstruct lambda_id: nil,
+    executed_at: nil,
+    result: nil,
+    output: "",
+    sign: nil,
+    duration: 0,
+    max_cpu_used: 0,
+    request_id: "",
+    status: nil
 
+  def put_duration(%__MODULE__{} = execution, duration) do
+    %{execution | duration: duration}
+  end
+
+  def put_result(%__MODULE__{} = execution, {:ok, result}) do
+    %{execution | result: result}
+    |> set_success
+  end
+  def put_result(%__MODULE__{} = execution, {:error, error}) do
+    %{execution | result: error}
+    |> set_error()
+  end
+
+  def put_output(%__MODULE__{} = execution, output) do
+    %{execution | output: output}
+  end
+
+  def set_success(%__MODULE__{} = execution) do
+    %{execution | status: :success}
+  end
+  def set_error(%__MODULE__{} = execution) do
+    %{execution | status: :error}
+  end
+
+  def put_executed_at(%__MODULE__{} = execution, time) do
+    %{execution | executed_at: time}
+  end
 end
--- apps/lambdex_core/lib/lambdex_core/lambda_supervisor.ex
+++ apps/lambdex_core/lib/lambdex_core/lambda_supervisor.ex
@@ -1,6 +1,8 @@
 defmodule LambdexCore.LambdaSupervisor do
   use GenServer
 
+  alias LambdexCore.Execution
+
   Process.flag(:trap_exit, true)
 
   def start_link(lambda, opts \\ []) do
@@ -10,14 +12,19 @@ defmodule LambdexCore.LambdaSupervisor do
   @impl true
   def init(lambda) do
     #start_supervised_task(lambda)
+    state = %{
+      lambda: lambda,
+      execution: %Execution{}
+    }
 
-    {:ok, lambda}
+    {:ok, state}
   end
 
   @impl true
   def handle_info({:DOWN, _ref, :process, pid, reason}, state) do
     IO.puts "#{inspect(reason)} by #{inspect(pid)}"
-    {:stop, reason, state}
+    #{:stop, reason, state}
+    {:noreply, state}
   end
   def handle_info(msg, state) do
     IO.inspect msg, label: "msg"
@@ -25,9 +32,26 @@ defmodule LambdexCore.LambdaSupervisor do
   end
 
   @impl true
-  def handle_call(:run, _from, lambda) do
-    result = Kernel.apply(lambda.code, lambda.params)
-    {:reply, result, lambda}
+  def handle_call(:run, _from, state) do
+    start_time = System.monotonic_time(:milliseconds)
+    at_time = :os.system_time(:seconds)
+
+    result =
+      try do
+        {:ok, Kernel.apply(state.lambda.code, state.lambda.params)}
+      rescue
+        error ->
+          {:error, error}
+      end
+
+    duration = System.monotonic_time(:milliseconds) - start_time
+    execution =
+      state.execution
+      |> Execution.put_executed_at(at_time)
+      |> Execution.put_duration(duration)
+      |> Execution.put_result(result)
+
+    {:reply, execution, %{state | execution: execution}}
   end
 
   ############
