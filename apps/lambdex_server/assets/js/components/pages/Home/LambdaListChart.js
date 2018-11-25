5772634a182d4eec700b4c6bcc74cac9460e14e2
mlambir <mlambir@gmail.com>
2018-11-25 12:22:17 -0300

    disable home chart animations

--- apps/lambdex_server/assets/js/components/pages/Home/LambdaListChart.js
+++ apps/lambdex_server/assets/js/components/pages/Home/LambdaListChart.js
@@ -5,7 +5,7 @@ import {LineChart, Line, Tooltip} from "recharts";
 const LambdaListChart = ({data, dataKey}) => (
   <div className={"lambda-list-chart"}>
     <LineChart width={200} height={50} data={data}>
-      <Line type="monotone" dataKey={dataKey} stroke="#8884d8" strokeWidth={2} activeDot={{r: 6}} />
+      <Line animationDuration={0} isAnimationActive={false} type="monotone" dataKey={dataKey} stroke="#8884d8" strokeWidth={2} activeDot={{r: 6}} />
       <Tooltip />
     </LineChart>
   </div>
